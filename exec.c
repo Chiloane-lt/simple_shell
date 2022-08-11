1a2c430f45aa26e63838bae1b89fab587b5fd070
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-11 03:30:55 +0000

    betty linter check

--- exec.c
+++ exec.c
@@ -9,25 +9,26 @@
 */
 int execute(char **cmd)
 {
-  pid_t child_pid;
-  int status;
-  
-  child_pid = fork();
-  
-  if (child_pid == -1)
-  {
-    perror("Error");
-    return (1);
-  }
-  else if (child_pid == 0)
-  {
-    if (execve(cmd[0], cmd, NULL) == -1)
-    {
-      perror("Error");
-      exit(-1);
-    }
-  }
-  else
-    wait(&status);
-  return (0);
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	else if (child_pid == 0)
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+	}
+	else
+		wait(&status);
+
+	return (0);
 }
