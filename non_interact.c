eba88bf753a166e64f1c276b81031190431f2878
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2022-08-11 03:05:28 +0200

    Create non_interact.c
    adding

---
+++ non_interact.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* non_interact - initiative of arguments
+* @av: pointer to strings with all arguments
+*
+* Return: void
+*/
+int non_interact(char **av)
+{
+  char error_msg[100];
+  
+  sprintf(error_msg, "%s: 0: Can't open %s\n", av[0], av[1]);
+  write(2, error_msg, strlen(error_msg));
+  return (127);
+}
