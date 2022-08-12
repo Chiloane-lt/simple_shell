c36fe6c8e08bc325414232ece93230ec05866de7
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-12 17:41:39 +0000

    Add comments for editing

--- stringToTokens.c
+++ stringToTokens.c
@@ -29,12 +29,22 @@ char **StringToTokens(char *str)
 int i = 0;
 const char separator[] = " ";
 int spaces = nbr_spaces(str);
+
 char **tokens = malloc(sizeof(char *) * (spaces + 1));
+/**Buffer created will be (soaces + 1) in size
+ * e.g.  the string was : "This is some random string" the buffer
+ * created will be 4 bytes insize. That's enough only to accommodate the
+ * first word. Not the rest of the string which will cause a segmentation
+ * fault*/
+
 char *tokens;
   
 if (!tokens)
 {
 fprintf(stderr, "sh: allocation error\n");
+/**The fprintf function is not allowed. alternatuvely
+ * you can put the string into a pointer and use puts or _putchar*/
+
 exit(1);
 }
 token = strtok(str, separator);
